a5403453c3dfa05d558a7dd11c60826209d3601a
basharatoum <atoumbashar@gmail.com>
2020-03-24 20:45:37 -0500

    fixes

--- XePA.py
+++ XePA.py
@@ -1,5 +1,6 @@
 from tables import *
+import os
 
-def getAPA():
-    APA = open_file("Xenon_Plus_Anything.h5")
+def getXePA():
+    APA = open_file(os.path.join(os.path.dirname(os.path.realpath(__file__)),"Xenon_Plus_Anything.h5"))
     return APA
