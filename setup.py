08848143c24a6387561713f59018014b7ef5aa2c
basharatoum <atoumbashar@gmail.com>
2020-03-24 19:10:18 -0500

    fixes

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='ArXe_plus_anythin',  # Required
+    name='ArXe_plus_anything',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
